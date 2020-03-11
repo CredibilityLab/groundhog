CredibilityLab/groundhog

commit 176dcead0fde5cc52da3701d116cecf829ba7f70

Do not use subset for non-interactive use

R/get.date.for.install.binary.R
@@ -15,7 +15,7 @@ get.date.for.install.binary=function(pkg_vrs)
   R.toc$path=sapply(  strsplit(R.toc$Version,"\\."), `[`, 3)
 
   #Find first and last current major.minor
-  R.same.minor=subset(R.toc,major==r.using.major & minor==r.using.minor )
+  R.same.minor=R.toc[R.toc$major == r.using.major && R.toc$minor == r.using.minor, ]
   k0=match(R.same.minor[1,]$Version,R.toc$Version)
   k1=match(R.same.minor[nrow(R.same.minor),]$Version,R.toc$Version)
 

R/cross.toc.R
@@ -16,6 +16,6 @@ cross.toc=function(pkgs,date1="1970-1-1",date2=Sys.Date())
   #Sort
   toc.all=toc.all[order(toc.all$Published),]
   #date subset
-  return(subset(toc.all,Published>date1 & Published<date2))
+  return(toc.all[toc.all$Published > date1 & toc.all$Published < date2, ])
 
 }

R/get.dependencies.R
@@ -5,7 +5,7 @@ get.dependencies=function(pkg,date,include.suggests=FALSE)
   #Get version from date
   vrs=get.version(pkg,date)
   #Get dependencies if version exists
-  row=subset(cran.toc,Package==pkg & Version==vrs)[,c("Imports","Depends","Suggests")]   #row in mastertoc
+  row=cran.toc[cran.toc$Package == pkg & cran.toc$Version == vrs,c("Imports","Depends","Suggests")]   #row in mastertoc
   dep=c(row$Imports,row$Depends)                      #merge
   if (include.suggests) dep=paste0(dep,row$Suggests)  #add 'Suggests' dependencies if requested
   dep=unlist(strsplit(dep,","))                      #turn to array

R/get.installation.time.R
@@ -1,6 +1,6 @@
 #' Get installation time
 get.installation.time=function(pkg,vrs)         {
-  dfk=subset(cran.times,pkg_vrs == paste0(pkg,"_",vrs))       #subset of package
+  dfk=cran.times[cran.times$pkg_vrs == paste0(pkg, "_", vrs), ]       #subset of package
   if (nrow(dfk)==1) return(dfk$installation.time)             #lookup installation times
   if (nrow(dfk)!=1) return(180)                               #if not found, assume 3 minutes
 }

R/get.pkg_search_paths.R
@@ -10,7 +10,9 @@ get.pkg_search_paths=function(pkg,vrs)
 
   #Subset of R.toc for same minor as r.using
   rv=r.version.check("2019-01-01")  #use arbitrary date for we don't use 'r.need', just r.using
-  subset.R.toc=subset(R.toc,minor==rv$r.using.minor & major==rv$r.using.major & Published<=get.rdate())
+  subset.R.toc=R.toc[R.toc$minor == rv$r.using.minor &
+                     R.toc$major==rv$r.using.major &
+                     R.toc$Published<=get.rdate(), ]
 
   #Sort versions from most recent
   subset.R.toc=subset.R.toc[order(subset.R.toc$Published,decreasing=T),]

R/install.snowball.R
@@ -13,7 +13,7 @@ install.snowball=function(pkg,date, include.suggests, force.install=FALSE,force.
   #1. FORCE INSTALL
   if (force.install==TRUE & sum(snowball$installed)>0) {
     #Subset of packages that are installed
-    snowball.installed=subset(snowball,installed==T)
+    snowball.installed= snowball[snowball$installed, ]
     #Get their path
     snowball.installed$paths=mapply(get.installed_path,snowball.installed$pkg,snowball.installed$vrs)
     #Rename the paths so they are not found and installation takes place

R/toc.R
@@ -1,8 +1,8 @@
 #' Show table of contents (toc) (package versions and publication dates) for a pkg, sorted chronologically
 toc=function(pkg,dependencies=FALSE) {
   if (!exists("cran.toc")) load.cran.toc(update.toc=FALSE)
-  if ( dependencies)  output=subset(cran.toc,Package==pkg)[,c("Version","Published","Imports","Depends","Suggests")]
-  if (!dependencies)  output=subset(cran.toc,Package==pkg)[,c("Version","Published")]
+  if ( dependencies)  output=cran.toc[cran.toc$Package == pkg,c("Version","Published","Imports","Depends","Suggests")]
+  if (!dependencies)  output=cran.toc[cran.toc$Package == pkg,c("Version","Published")]
 
   if (nrow(output)==0) {
     cat2()

R/utils.R
@@ -11,8 +11,7 @@ current.packages$pkg_vrs=paste0(current.packages$Package,"_",current.packages$Ve
 
 #1.3 base packages
 #' @importFrom utils installed.packages
-ip=data.frame(installed.packages())
-dep.base=as.character(subset(ip,Priority=='base')$Package)
+dep.base = installed.packages(priority = "base")
 
 #2.0.5 Parse pkg_vrs into pkg and vrs
 get.pkg=function(x) substr(x,1,regexpr('_', basename(x))-1)

commit fd453246fd76d591ea92e97fd41d3e716a3f057f

Fix typo introduced in 176dcead0fde5cc52da3701d116cecf829ba7f70

R/get.date.for.install.binary.R
@@ -15,7 +15,7 @@ get.date.for.install.binary=function(pkg_vrs)
   R.toc$path=sapply(  strsplit(R.toc$Version,"\\."), `[`, 3)
 
   #Find first and last current major.minor
-  R.same.minor=R.toc[R.toc$major == r.using.major && R.toc$minor == r.using.minor, ]
+  R.same.minor=R.toc[R.toc$major == r.using.major & R.toc$minor == r.using.minor, ]
   k0=match(R.same.minor[1,]$Version,R.toc$Version)
   k1=match(R.same.minor[nrow(R.same.minor),]$Version,R.toc$Version)
 