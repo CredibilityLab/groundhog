640c4de5d2c9511cc00b2dd9aace8bcc388a1c6d
Hugo Gruson <hugo.gruson@protonmail.com>
2020-03-27 11:22:44 +0100

    Abort early if date is in the future

--- R/update_cran.toc_if.needed.R
+++ R/update_cran.toc_if.needed.R
@@ -4,6 +4,19 @@ update_cran.toc_if.needed <- function(date) {
   # 1 Format entered date by user
   date <- as.DateYMD(date)
 
+  # Stop if date is in the future
+  if (date > Sys.Date() - 2) {
+    message2()
+    message1(
+      "GroundhogR's database is updated multiple times a day, but, to ensure",
+      " reproducibility of your script, given time zone differences and ",
+      "delays in updating different CRAN mirrors, don't use a date more ",
+      "recent than two days ago, (i.e., the most recent date you may use ",
+      "today with groundhogR is:'", Sys.Date() - 2, "')."
+    )
+    stop()
+  }
+
   # 2 Load cran.toc if not yet loaded
   if (is.null(.pkgenv[["cran.toc"]])) {
     load.cran.toc(update.toc = FALSE)
@@ -22,19 +35,7 @@ update_cran.toc_if.needed <- function(date) {
       "The date you entered, '", date, "', requires updating your database with the list of CRAN package versions, \n",
       " for it goes only until ", max.date, ".The udate is happening as you read this. "
     )
-
-    # Stop if date is in the future
-    msg.future.date <- paste0(
-      "GroundhogR's database is updated multiple times a day, but, to ensure reproducibility of your script,",
-      "given time zone differences and delays in updating different CRAN mirrors, don't use a date more recent than ",
-      "two days ago, (i.e., the most recent date you may use today with groundhogR is:'", Sys.Date() - 2, "')."
-    )
-    if (date > Sys.Date() - 2) {
-      message2()
-      message1(msg.future.date)
-      stop()
-    }
     # Update the database
     load.cran.toc(TRUE)
   } # End if desired date is after most recent date
-} # End 2.12
+}
