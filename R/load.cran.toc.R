CredibilityLab/groundhog

commit bd6d7d5824297a933eee498b5c6ce12aac40dbca

Test class with inherits https://developer.r-project.org/Blog/public/2019/11/09/when-you-think-class.-think-again/index.html

R/load.cran.toc.R
@@ -49,7 +49,7 @@ load.cran.toc <- function(update.toc = FALSE) {
     add.toc <- try(read.csv(paste0(groundhogR.url, "/differential.toc.php?current_date=", max.existing.toc.date))[-1])
 
     # 3.3.5 If sucess loading URL
-    if (class(add.toc) == "data.frame") {
+    if (is.data.frame(add.toc)) {
       # Get pkg_vrs for packages to add
       add.toc.pkg_vrs <- paste0(add.toc$Package, "_", add.toc$Version)
 
@@ -72,7 +72,7 @@ load.cran.toc <- function(update.toc = FALSE) {
 
 
     # 3.3.7 If sucess loading URL
-    if (class(add.times) == "data.frame") {
+    if (is.data.frame(add.times)) {
       # Get pkg_vrs for packages to add
       add.times.pkg_vrs <- paste0(add.times$Package, "_", add.times$Version)
 
@@ -96,13 +96,13 @@ load.cran.toc <- function(update.toc = FALSE) {
       "This computer had a database with a list of all versions available for each CRAN package up to ",
       max.existing.toc.date + 2, " for a total of N=", nrow(existing.toc), " package versions."
     )) # Add back the two days we took out
-    if (class(add.toc) == "data.frame") {
+    if (is.data.frame(add.toc)) {
       cat1(paste0(
         "We checked for additions to CRAN since then, and added ",
         nrow(add.toc.net), " additional entries to the list.\n"
       ))
     }
-    if (class(add.toc) != "data.frame") {
+    if (is.data.frame(add.toc)) {
       cat1(paste0("We tried to update till today but it did not work"))
     }
 
@@ -118,7 +118,7 @@ load.cran.toc <- function(update.toc = FALSE) {
     d2 <- try(download.file(times.url, times.path))
 
     # 3.4.2 if it fails (d1), give bad news
-    if (class(d1) == "try-error") {
+    if (inherits(d1, "try-error")) {
       cat2()
       cat1(paste0(
         "Could not load the database of packages available in CRAN neither locally from this\n",

R/install.source.R
@@ -30,28 +30,28 @@ install.source <- function(pkg_vrs, lib, date, force.download = FALSE, quiet = F
 
 
     # 6.4.3 If downloading fails, try from MRAN on date requested as current, then that date as archive, then first day on MRAN as current, then archive
-    if (class(down.tarball.attempt) == "try-error") {
+    if (inherits(down.tarball.attempt, "try-error")) {
       down.tarball.attempt <- try(download.file(paste0("https://cran.microsoft.com/snapshot/", date, "/src/contrib/", pkg_vrs, ".tar.gz"), destfile = tarball.path))
     }
-    if (class(down.tarball.attempt) == "try-error") {
+    if (inherits(down.tarball.attempt, "try-error")) {
       down.tarball.attempt <- try(download.file(paste0("https://cran.microsoft.com/snapshot/", date - 2, "/src/contrib/Archive/", pkg, "/", pkg_vrs, ".tar.gz"), destfile = tarball.path))
     }
-    if (class(down.tarball.attempt) == "try-error") {
+    if (inherits(down.tarball.attempt, "try-error")) {
       down.tarball.attempt <- try(download.file(paste0("https://cran.microsoft.com/snapshot/", date - 2, "/src/contrib/", pkg_vrs, ".tar.gz"), destfile = tarball.path))
     }
-    if (class(down.tarball.attempt) == "try-error") {
+    if (inherits(down.tarball.attempt, "try-error")) {
       down.tarball.attempt <- try(download.file(paste0("https://cran.microsoft.com/snapshot/", date + 1, "/src/contrib/Archive/", pkg, "/", pkg_vrs, ".tar.gz"), destfile = tarball.path))
     }
-    if (class(down.tarball.attempt) == "try-error") {
+    if (inherits(down.tarball.attempt, "try-error")) {
       down.tarball.attempt <- try(download.file(paste0("https://cran.microsoft.com/snapshot/", date + 1, "/src/contrib/", pkg_vrs, ".tar.gz"), destfile = tarball.path))
     }
-    if (class(down.tarball.attempt) == "try-error") {
+    if (inherits(down.tarball.attempt, "try-error")) {
       down.tarball.attempt <- try(download.file(paste0("https://cran.microsoft.com/snapshot/", date, "/src/contrib/Archive/", pkg, "/", pkg_vrs, ".tar.gz"), destfile = tarball.path))
     }
-    if (class(down.tarball.attempt) == "try-error") {
+    if (inherits(down.tarball.attempt, "try-error")) {
       down.tarball.attempt <- try(download.file(paste0("https://cran.microsoft.com/snapshot/2014-09-18/src/contrib/", pkg_vrs, ".tar.gz"), destfile = tarball.path))
     }
-    if (class(down.tarball.attempt) == "try-error") {
+    if (inherits(down.tarball.attempt, "try-error")) {
       down.tarball.attempt <- try(download.file(paste0("https://cran.microsoft.com/snapshot/2014-09-18/src/contrib/Archive/", pkg, "/", pkg_vrs, ".tar.gz"), destfile = tarball.path))
     }
   }

commit 2b1a754880cfa2b0f8ab95ce8808374f4f780bbf

Simplify if/else structure and fix typo introduced in bd6d7d5824297a933eee498b5c6ce12aac40dbca

R/load.cran.toc.R
@@ -100,8 +100,7 @@ load.cran.toc <- function(update.toc = FALSE) {
         "We checked for additions to CRAN since then, and added ",
         nrow(add.toc.net), " additional entries to the list.\n"
       )
-    }
-    if (is.data.frame(add.toc)) {
+    } else {
       message1("We tried to update till today but it did not work")
     }
 