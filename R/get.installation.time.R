790bb68dbd0d3a5194f9067ce7115903c7e24f55
Hugo Gruson <hugo.gruson@protonmail.com>
2020-04-11 19:37:36 +0200

    Add tests for timing function

--- R/get.installation.time.R
+++ R/get.installation.time.R
@@ -15,6 +15,9 @@
 #'
 get.installation.time <- function(pkg, vrs) {
 
+  if (is.null(.pkgenv[["cran.times"]])) {
+    load.cran.toc()
+  }
   cran.times <- .pkgenv[["cran.times"]]
 
   dfk <- cran.times[cran.times$pkg_vrs == paste0(pkg, "_", vrs), ] # subset of package
