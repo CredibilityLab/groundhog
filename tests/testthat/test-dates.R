fee65ce396cc050e282be993193314ea142f095a
Hugo Gruson <hugo.gruson@protonmail.com>
2020-04-13 19:36:35 +0200

    Remove broken expect_silent() On first run, messages are expected to set things up

--- tests/testthat/test-dates.R
+++ tests/testthat/test-dates.R
@@ -3,7 +3,7 @@ test_that("get.R.pkg.date()", {
   test_pkg <- "lightr_1.0"
   test_r <- "3.6.0"
 
-  pkg_date <- expect_silent(get.R.pkg.date(test_pkg, test_r))
+  pkg_date <- get.R.pkg.date(test_pkg, test_r)
 
   expect_is(pkg_date, "Date")
   expect_true(pkg_date <= Sys.Date())
@@ -14,7 +14,7 @@ test_that("get.date.for.install.binary()", {
 
   test_pkg <- "lightr_1.0"
 
-  bin_date <- expect_silent(get.date.for.install.binary(test_pkg))
+  bin_date <- get.date.for.install.binary(test_pkg)
 
   expect_is(bin_date, "Date")
   expect_true(bin_date <= Sys.Date())
@@ -49,9 +49,8 @@ test_that("get.version()", {
   test_pkg <- "lightr"
   test_date <- "2020-01-01"
 
-  expect_silent(
-    vrs <- get.version(test_pkg, test_date)
-  )
+  vrs <- get.version(test_pkg, test_date)
+
   expect_is(vrs, "character")
 
   test_before <- "2010-01-01"
