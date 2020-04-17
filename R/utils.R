0b53e9eca9d35625c55af039bf099e942e3acaf7
Hugo Gruson <hugo.gruson@protonmail.com>
2020-04-17 17:07:29 +0200

    Fix CRAN mirror setting

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,5 @@
-# 1. Preliminaries
-chooseCRANmirror(graphics = FALSE, ind = 1)
-
 # 1.2 Get available packages to see if each attempted to install is new
-current.packages <- data.frame(available.packages())[, c(1, 2)]
+current.packages <- data.frame(available.packages(contriburl = contrib.url(repos = "https://cran.r-project.org/")))[, c(1, 2)]
 current.packages$pkg_vrs <- paste0(current.packages$Package, "_", current.packages$Version)
 
 # 2.0.5 Parse pkg_vrs into pkg and vrs
