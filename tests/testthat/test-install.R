7841ef737fd9c93ab75d7a6f23c486c9badc0544
Hugo Gruson <hugo.gruson@protonmail.com>
2020-04-19 15:57:04 +0200

    Add test for groundhog.library()

---
+++ tests/testthat/test-install.R
@@ -0,0 +1,18 @@
+test_that("groundhog.library()", {
+
+  tmp_gdir <- file.path(tempdir(), "groundhogR")
+  dir.create(tmp_gdir)
+
+  file.copy("../../_tarballs/",
+            file.path(tmp_gdir),
+            recursive = TRUE)
+
+  Sys.setenv("GROUNDHOGR_FOLDER" = tmp_gdir)
+
+  res <- groundhog.library("magrittr", "2020-01-01")
+
+  expect_true("stringr_1.4.0" %in% res)
+
+  Sys.unsetenv("GROUNDHOGR_FOLDER")
+
+})
