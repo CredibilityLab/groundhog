bf4768a298544751748256965f80f11ecfd24a45
Hugo Gruson <hugo.gruson@protonmail.com>
2020-04-28 11:34:11 +0200

    Revert "Fix the confusion between 'lib' and 'destdir'  in instal.packages()"
    This reverts commit 8877ca425e44bc0dc89de4d9928e7121347903ee.

--- R/install.source.R
+++ R/install.source.R
@@ -75,7 +75,7 @@ install.source <- function(pkg_vrs, lib, date, force.download = FALSE, quiet.ins
     # Create the folder
     dir.create(lib, showWarnings = FALSE, recursive = TRUE)
     # Install the package
-    install.packages(tarball.path, type = "source", lib = .pkgenv[["libPaths"]], destdir = lib, quiet = quiet.install, dependencies = FALSE, repos = NULL, INSTALL_opts = "--no-staged-install")
+    install.packages(tarball.path, type = "source", lib = lib, quiet = quiet.install, dependencies = FALSE, repos = NULL, INSTALL_opts = "--no-staged-install")
   } # End if success
 
 } # End of install.source
