3f73d4742c0d9aedd3daee8987064ca00e111028
Hugo Gruson <hugo.gruson@protonmail.com>
2020-05-09 19:17:13 +0200

    Add initial check for newer version partial fix for #23

--- R/zzz.R
+++ R/zzz.R
@@ -2,14 +2,24 @@
 
 .pkgenv <- new.env(parent = emptyenv())
 
+#' @importFrom utils packageVersion
+#'
 .onLoad <- function(libname, pkgname) {
   .pkgenv[["supportsANSI"]] <- Sys.getenv("TERM") %in% c("xterm-color", "xterm-256color", "screen", "screen-256color")
 
   # 1.2 Get available packages to see if each attempted to install is new
   current.packages <- as.data.frame(
-    available.packages(contriburl = contrib.url(repos = "https://cran.r-project.org/"))[, c(1, 2)]
+    available.packages(contriburl = contrib.url(repos = "https://cran.r-project.org/"))[, c(1, 2)],
+    stringsAsFactors = FALSE
   )
   current.packages$pkg_vrs <- paste0(current.packages$Package, "_", current.packages$Version)
 
   .pkgenv[["current.packages"]] <- current.packages
+
+  if (any(package_version(current.packages$Version) > packageVersion("groundhogR"))) {
+    packageStartupMessage(
+      "A more recent version of groundhogR is available. Please install it by ",
+      'running install.packages("groundhogR") and then restart your R session.'
+    )
+  }
 }
