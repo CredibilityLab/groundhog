6ce5ad19515949e38ac681eacaddd5183cb85ea0
Hugo Gruson <hugo.gruson@protonmail.com>
2020-09-06 21:50:22 +0200

    Add explicit error message when running offline

--- R/get.current.packages.R
+++ R/get.current.packages.R
@@ -1,10 +1,17 @@
 get.current.packages <- function(type) {
 
   # Get available packages to see if each attempted to install is new
-  current.packages <- as.data.frame(
-    available.packages(contriburl = contrib.url(repos = "https://cloud.r-project.org/", type = type))[, c(1, 2)],
-    stringsAsFactors = FALSE
-  )
+  current.packages <- tryCatch({
+    as.data.frame(
+      available.packages(contriburl = contrib.url(repos = "https://cloud.r-project.org/", type = type))[, c(1, 2)],
+      stringsAsFactors = FALSE
+    )},
+    error = function(e) NULL)
+
+  if (is.null(current.packages)) {
+    exit("groundhog failed to reach the CRAN server. Are you running offline?")
+  }
+
   current.packages$pkg_vrs <- paste0(current.packages$Package, "_", current.packages$Version)
 
   return(current.packages)
