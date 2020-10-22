3d170369a68f5136e0aeb4341125666836d72947
Hugo Gruson <hugo.gruson@protonmail.com>
2020-10-22 11:51:38 +0200

    Fix patch version number in message

--- R/installation.feedback.R
+++ R/installation.feedback.R
@@ -39,7 +39,7 @@ installation.feedback <- function(k, date, snowball, start.time) {
     msg <- c(
       msg,
       "\n> Installation is slow because you are using R-", get.rversion(), "\n",
-      "> If you run this script with the R version current for the date you entered: '", date, "', i.e., R-", get.version("R", date), ",\n",
+      "> If you run this script with the R version current for the date you entered: '", date, "', i.e., R-", rv$r.need.patch, ",\n",
       "  the entire installation would take about a few minutes at most.\n",
       "> Instructions for running other versions of R on demand:  https://groundhogR.com/many"
     )
