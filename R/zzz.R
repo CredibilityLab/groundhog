026d9e3ddb86e5d6ad700c77ac1671ee8f8e6a7b
Hugo Gruson <hugo.gruson@protonmail.com>
2020-11-16 15:42:02 +0100

    Fix test on macOS

--- R/zzz.R
+++ R/zzz.R
@@ -19,7 +19,7 @@
   # isTRUE() is necessary here because this will return logical(0) if the pkg
   # is not on CRAN, or if working offline (current.packages is NULL in this case).
   groundhog.version_using <- as.character(packageVersion("groundhog"))
-  if (isTRUE(compareVersion(groundhog.version_cran, groundhog.version_using) > 0)) {
+  if (isTRUE(groundhog.version_cran > groundhog.version_using)) {
     packageStartupMessage(
       'groundhog says: "OUTDATED"\nYou are using version ', groundhog.version_using, " and the current version is ",
       groundhog.version_cran, '. Please update by running: \ninstall.packages("groundhog")'
