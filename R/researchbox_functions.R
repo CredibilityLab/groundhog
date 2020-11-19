c49498b4276b52f58b6be65ee10a6dd2ec5e13ec
Hugo Gruson <hugo.gruson@protonmail.com>
2020-11-19 10:07:55 +0100

    Update documentation

--- R/researchbox_functions.R
+++ R/researchbox_functions.R
@@ -1,16 +1,8 @@
-
-
 # FROM https://stackoverflow.com/questions/1815606/determine-path-of-the-executing-script -
 # Specifically: https://stackoverflow.com/a/36777602/14647087
-#' current script file (in full path)
-#' @description current script file (in full path)
-#' works with Rscript, source() or in RStudio Run selection, RStudio Console
-#'
-#' @examples
-#' setwd(researchbox_data())
-#' x <- researchbox_other()
-#'
-#' @export
+#
+# works with Rscript, source() or in RStudio Run selection, RStudio Console
+#
 get.script.folder <- function() {
   # http://stackoverflow.com/a/32016824/2292993
   cmdArgs <- commandArgs(trailingOnly = FALSE)
@@ -53,6 +45,26 @@ get.script.folder <- function() {
   }
 }
 
+#' Get researchbox project paths
+#'
+#' Get paths relative to the different subfolders of a
+#' [researchbox](https://researchbox.org/).
+#'
+#' @return
+#' - For `researchbox_code()`, the path to the `Code` folder of the researchbox
+#' - For `researchbox_data()`, the path to the `Data` folder of the researchbox
+#' - For `researchbox_other()`, the path to the `Other` folder of the
+#' researchbox
+#'
+#' @examples
+#' x <- researchbox_other()
+#' \dontrun{
+#' setwd(researchbox_data())
+#' }
+#'
+#' @export
+#'
+#' @rdname researchbox
 researchbox_code <- function() {
 
   return(dirname(get.script.folder()))
@@ -66,6 +78,8 @@ researchbox_dir <- function() {
   return(researchbox_dir)
 }
 
+#' @rdname researchbox
+#' @export
 researchbox_data <- function() {
 
   data_dir <- file.path(researchbox_dir(), "Data")
@@ -80,6 +94,8 @@ researchbox_data <- function() {
   return(data_dir)
 }
 
+#' @rdname researchbox
+#' @export
 researchbox_other <- function() {
 
   other_dir <- file.path(researchbox_dir(), "Other")
