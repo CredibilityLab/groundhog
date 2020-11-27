b85c97f57990cc93963e80d4e8745e713c262769
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2020-11-27 10:43:41 +0100

    MAJOR - rewrote get.binary.from.date()

--- R/get.date.for.install.binary.R
+++ R/get.date.for.install.binary.R
@@ -18,29 +18,39 @@ get.date.for.install.binary <- function(pkg_vrs) {
   r.using.major <- R.version$major
   r.using.minor <- strsplit(R.version$minor, "\\.")[[1]][1]
 
-  # Get R Toc
-  R.toc <- toc("R") # Get R toc
+  #1 Find R1 and R2 (first and last dates with the current vesion of R (ignoring patch)
+  
+    #1.1 Get all R releases
+      R.toc <- toc("R") # Get R toc
 
-  # Find all R versions with same major and minor versions as currently using
-  R_vrs <- grep(paste0("^", r.using.major, ".", r.using.minor), R.toc$Version, value = TRUE)
+    #1.2  Find all R versions with same major and minor versions as currently using
+      R_vrs <- grep(paste0("^", r.using.major, ".", r.using.minor), R.toc$Version, value = TRUE)
 
-  # Look through them starting with the most recent one
-  R_vrs <- rev(R_vrs)
+    #1.3 Get the first and last one
+      R1=R_vrs[1]
+      R2=R_vrs[length(R_vrs)]
+      
+    #1.4 Get those dates
+      date.R1=R.toc[match(R1,R.toc$Version),]$Published
+      date.R2=R.toc[match(R2,R.toc$Version),]$Published
 
-  # And put the currently used version first
-  R_vrs <- c(r.using.full, R_vrs[R_vrs != r.using.full])
+  #2 Fine pkg1 and pkg2 (first and last dates with the desired package version
+    #2.1 Get all package releases
+      pkg=get.pkg(pkg_vrs)
+      pkg.toc <- toc(pkg) 
 
-  for (v in R_vrs) {
-    binary.date <- get.R.pkg.date(pkg_vrs, v)
-    if (binary.date > "1970-01-01") {
-      break
-    }
-  }
-
-  # If date if from before MRAN's first day, 2014-09-17, go to 1970
-  if (binary.date < "2014-09-17") {
-    binary.date <- as.DateYMD("1970-01-01")
-  }
-
+    #2.2 Match the date of desired, and grab that and next release as date.pkg1, date.pkg2
+      vrs=get.vrs(pkg_vrs)
+      date.pkg1 <- pkg.toc[match(vrs,pkg.toc$Version),]$Published    #date when desired version was released
+      date.pkg2 <- pkg.toc[match(vrs,pkg.toc$Version)+1,]$Published  #date when next version was released
+      
+      #If no next release, use 'today' minus two days
+      if (is.na(date.pkg2)) {
+          date.pkg2 <- Sys.Date()-2
+          }
+      
+  #3 The date to use is day prior to either the new package or the new R release
+      binary.date <- min(date.R2 - 1, date.pkg2 -1)
+      
   return(as.DateYMD(binary.date))
 }
