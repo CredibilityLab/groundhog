57a4d1e7a3612b10f94d69c4047f67f9f679a8ec
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2020-12-04 15:32:14 +0100

    install.feedback() add same package seconds estimate if >180

--- R/installation.feedback.R
+++ R/installation.feedback.R
@@ -29,26 +29,26 @@ installation.feedback <- function(k, date, snowball, start.time) {
 	  msg.header <- paste0("\n\n\ngroundhog says: Installing '", snowball[k, "pkg_vrs"], "', package #", k, 
 	                       " (from source) out of ", N, " needed")
 	  msg <- c(
-		"> As of ", format(Sys.time(), "%H:%M"), ", the  best guess is that all ", N," packages will be installed ",
-		  "around ",   finish.time.estimate,".\n",
-		"  It is somewhat unlikely (but not impossible) for the process to last past ", finish.time.max, "\n",
-		"> Estimates are revised after each package installs, but will remain noisy throughout"
-	  )
+    		"> As of ", format(Sys.time(), "%H:%M"), ", the best guess is that all ", N," packages will be installed ",
+    		  "around ",   finish.time.estimate,".\n",
+    		"  It is somewhat unlikely (but not impossible) for the process to last past ", finish.time.max, "\n",
+    		"> Estimates are revised after each package installs, but will remain noisy throughout")
+	
+	  
 
-  # Add msg if R mismatch and recent enough for groundhog
-	  if (rv$r.using.majmin != rv$r.need.majmin & date > "2014-09-18") {
+  # Add msg if R mismatch and recent enough for groundhog and using Windows or Mac
+	  if (rv$r.using.majmin != rv$r.need.majmin & date > "2014-09-18" & .Platform$pkgType != "source") {
 		msg <- c(
 		  msg,
 		  "\n> Installation is slow because you are using R-", rv$r.using.full,", a major update on the version\n",
 		  " available on the requested date: '",date,"'.\n",
 		  "> If you run this script with R-", rv$r.need.full, ", the installation would be faster.\n",
-		  "> Moreover, some scripts will give different results in different versions of R, thus you may want \n",
-		  "  to run this script with R-", rv$r.need.full, "  if you don't obtain expected results.\n",
+		  "> Moreover, note that some scripts will give different results in different versions of R.\n",
 		  "> Instructions for running previous versions of R:  https://groundhogR.com/many"
 		)
 	  } # End if R mismatch
 
-  # Add message if installing from source taht output has been suppressed
+  # Add message if installing from source that output has been suppressed
 	  if (snowball[k, "from"] == "source") {
 		msg <- c(
 		  msg,
@@ -56,7 +56,18 @@ installation.feedback <- function(k, date, snowball, start.time) {
 		  " flooding the console where these messages are printed. Thus, groundhog.library() supresses\n",
 		  " such output. You may run groundhog.library() with the option 'quiet=FALSE' to display all output."
 		) # End of message
+		
+	#Add warning fo slow package
+		#Add estimate for this one package, unless it is the last one.
+	  if (snowball$installation.time[k]>120) {
+	        msg <- c(msg, "\n\n>       *PATIENCE*: This package ('", snowball$pkg_vrs[k],"') is expected to take about ",
+	       snowball$installation.time[k], " seconds.")  
+	       }
+	 
+		
 	  } # End of if source file
+	  
+	  
 
 
 
