48d04069d95f1f9fd8afbfd4e2147408a80eb806
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2022-01-21 06:24:22 +0100

    Update validate.pkg_vrs.R

--- R/validate.pkg_vrs.R
+++ R/validate.pkg_vrs.R
@@ -1,30 +1,35 @@
 
     
+  
+  validate.pkg_vrs <- function(pkg, vrs, date, ignore.deps)
+  { 
+  
+  #0 Merge pkg_vrs
+    pkg_vrs <- paste0(pkg, "_", vrs)
+  
     
-    validate.pkg_vrs <- function(pkg, vrs, date, ignore.deps)
-    { 
-    
-
-    #0. Don't try to load 'groundhog'
+  #1 No 'groundhog'
         if ("groundhog" == pkg) {
-          message("Error. May not use groundhog.library() to load groundhog.\n",
-                  "To load the version of groundhog available on '", date, "', please use:\n",
-                  "meta.groundhog('" ,  date , "')"
-                   )
-          exit()
-        } #End if groundhog is a package being called
+        message("groundhog says: Error. You may not use groundhog.library() to load groundhog.\n",
+                "To load the version of groundhog available on '", date, "', please use:\n",
+                "meta.groundhog('" ,  date , "')"
+                 )
+        exit()
+        } #End if groundhogd
 
-  
-    #1.0 initial check,  stop if same version
-      active=get.active()
+    
+     
+  #2 Read Active & libPaths 
+    #2.1 Read active
+       active <- get.active()
+       
+    #2.1 Read paths
+       paths  <- get.groundhog_libpaths()
+       paths.pkg_vrs <- basename(paths)
+       paths.pkg     <-  as.character(sapply(paths.pkg_vrs, function(x) { strsplit(x,"_")}[[1]][1]))
+        
       
-  
-    #1.1 Get version of requested package
-        #vrs <- get.version(pkg, date)
-        pkg_vrs <- paste0(pkg, "_", vrs)
-      
-
-    #1.2 Stop if  pkg_vrs already attached
+    #2.2 Read attached
         attached.list= utils::sessionInfo()$otherPkgs
         attached.pkg <- names(attached.list)
         attached.vrs <- lapply(attached.list, function(x) x$Version)
@@ -36,27 +41,22 @@
         attached.vrs <- c(attached.vrs, attached.base.vrs)
         attached.pkg_vrs <- paste0(attached.pkg , "_" , attached.vrs)
         
-        if (pkg_vrs %in% attached.pkg_vrs) {
-            message1("groundhog says: the package you requested ('", pkg_vrs, "') is already attached.")
-            exit()
-            
-        }
-
-  
-        
-    #1.2.5 Attach if package is loaded but not attached
-        if (pkg_vrs %in% active$pkg_vrs)
-        {
-          attachNamespace(pkg)
-          message1("groundhog says: the package you requested ('", pkg, "_", vrs, "') was loaded, now it is also attached")
-          exit()
-
-        }
-   
-   #1.3 Mismatched package already attached  
+  #3 Early return if already attached
+            if (pkg_vrs %in% attached.pkg_vrs) {
+                  message1("groundhog says: the package you requested ('", pkg_vrs, "') is already attached.")
+                  return('already_attached')  
+            }
+                
+              #Early return so that groundhog.library.single() knows to stop processing this pkg, 
+              #but if more where submitted in pkg=c(pkg1,pkg2) it will just move on to the next package
+              #instead of ending the entire groundhog.library() call
+      
+  #4 CONFLICT
+             
+   #4.1 Mismatched package already attached  
          if ((pkg %in% attached.pkg) &  (!pkg_vrs %in% attached.pkg_vrs)) {
             message1(
-                    "groundhog says: another version of '", pkg,"' is already attached ('", active$pkg_vrs[active$pkg==pkg],"').\n",
+                    "Another version of '", pkg,"' is already attached ('", active$pkg_vrs[active$pkg==pkg],"').\n",
                     "To solve this: restart the R session. Note: you will need to do 'library(groundhog)' again.\n\n",
                     "In R Studio press: CTRL/CMD-SHIFT-F10"
                     )
@@ -65,33 +65,43 @@
          }
         
     
-    #1.5 Attach mismatched version if ignore.deps is loaded but not attached (common scenario, trying to attach knitr in .rmd file)
-       if ((pkg %in% active$pkg) & (!pkg_vrs %in%  active$pkg_vrs) & (pkg %in% ignore.deps))
+    #4.2 Attach mismatched version if ignore.deps is loaded but not attached (common scenario, trying to attach knitr in .rmd file)
+       if ((pkg %in% active$pkg | pkg %in% paths.pkg) &                         #pkg matches
+           ((!pkg_vrs %in%  active$pkg_vrs) & (!pkg_vrs %in% paths.pkg_vrs))    #pkg_vrs does not
+            & (pkg %in% ignore.deps))                                           #in ignoreable conflict list.
         {
-         #Recommended
-             ip <- data.frame(utils::installed.packages(),stringsAsFactors = FALSE)
-             recommended.pkgs <- unique(subset(ip, ip$Priority=="recommended")$Package) #unique because there may be two versions of the same package in different libraries
-         
-          attachNamespace(pkg)
+           
+        #Attach it 
+          library(pkg , character.only = TRUE )
+             
+        #Make local variable with name of pkg_vrs already loaded oralready in libpath creating conflict
+             available.pkg_vrs <- ifelse(pkg  %in% active$pkg, active$pkg_vrs[active$pkg==pkg] , paths.pkg_vrs[paths.pkg==pkg])
+             
+        #Message  
           message1("groundhog says: succesfully attached '" , pkg , "'")
-          message("\ngroundhog warning:\n", 
-                   "'", pkg, "' was already loaded, and it is now attached,\n",
-                  "BUT the loaded version ('" , active$pkg_vrs[active$pkg==pkg] , "') does not match the version for ",
-                   "'" , date, "' ('", pkg_vrs , "').\n",
+          message("\ngroundhog says: Warning!\n", 
+                   "'", pkg, "' was already available in the R session and it is now attached,\n",
+                  "BUT the version previously available and just attached ('" ,available.pkg_vrs , "')\n",
+                  "does not match the version for '" , date, "' ('", pkg_vrs , "').\n",
                   "To attach the desired version you can try restarting the R session.\n\n",
                   "In R Studio press: CTRL/CMD-SHIFT-F10\n\n",
                   "Note that it is possible that this package is being loaded automatically from your local\n",
-                  "library. In this case, after restarting the session the problem will persist.\n",
+                  "library (e.g., by R Studio). In this case, after restarting the session the problem will persist.\n",
                   "You can ignore this problem and tolerate lack of version control for the involved packages.\n", 
-				  "You can also prevent this problem by uninstalling the package from your non-groundhog library\n",
-				  "running: remove.packages('", pkg ,"'), but if R Studio is using the package (e.g., the\n",
-				  "'knit' button requires 'knitr' outside of groundhog), you may need to rely on R rather than\n",
-				  "R Studio for that disabled functionality."
-				 )
+        				  "You can also prevent this problem by uninstalling the package from your non-groundhog library\n",
+        				  "running: remove.packages('", pkg ,"'), but if R Studio is using the package (e.g., the\n",
+        				  "'knit' button requires 'knitr' outside of groundhog), you may need to rely on R rather than\n",
+        				  "R Studio for that disabled functionality."
+        				 )
 		
           
-        #Explain the recommended issue if appropriate
-          if (pkg %in% recommended.pkgs) {
+        #Explain the recommended pkgs to highlight this may be trickier 
+             ip <- data.frame(utils::installed.packages(),stringsAsFactors = FALSE)
+             recommended.pkgs <- unique(subset(ip, ip$Priority=="recommended")$Package) 
+             
+               #Note:unique because there may be two versions of the same package in different libraries
+    
+            if (pkg %in% recommended.pkgs) {
               message(
                     "The package in question '", pkg , "', is a 'recommended' package which makes removing it from the\n",
                     "local (non-groundhog) library potentially more problematic; this is why this conflict\n",
@@ -101,6 +111,6 @@
           exit()
 
         }   
-        
+        return('')
         
     } #End function
