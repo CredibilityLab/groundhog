7ad52bf47f00e625afcc70e9369c59d663cf7acf
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2022-04-24 08:19:45 +0200

    short msg for conflict --> disable.local

--- R/check.snowball.conflict.R
+++ R/check.snowball.conflict.R
@@ -26,8 +26,7 @@ check.snowball.conflict <- function(snowball, force.install, ignore.deps, date) 
        
       #2.2 Active packages
         active <- get.active()
-        
-
+  
 
   #3 Force Install: is any package that needs to be *installed* is loaded?
     #separate check from because even SAME version created conflict
@@ -122,7 +121,7 @@ check.snowball.conflict <- function(snowball, force.install, ignore.deps, date) 
           }
 
             
-      #Add 'alterantively' before F10 if a msg about ignore.deps was shown before
+      #Add 'alternatively' before F10 if a msg about ignore.deps was shown before
         if (flag.conflict_needed_remote | flag.conflict_active_remote) message1("Alternatively, if you think you can avoid the conflict:")
      
          
@@ -144,8 +143,8 @@ check.snowball.conflict <- function(snowball, force.install, ignore.deps, date) 
   #8 Show fix, workaround message if same failure less than 5 minutes ago and not loading the conflicting package adn only 1 date was used and no package conflict is with remote
         if (last_conflict<5 & 
 					requested_pkg == last_conflict.pkg & 
-          flag.conflict_needed_remote==FALSE &  #Don't sugggest uninstalling if remote conflict
-          flag.conflict_active_remote==FALSE &  #Same concept, in one case the loaded is remote in the other
+          flag.conflict_needed_remote==FALSE &  #Don't suggest disabling if remote conflict
+          flag.conflict_active_remote==FALSE &  #Same concept, in one case the loaded is remote in the other the needed one
 					(length(.pkgenv[['hogdays']])<2)) 
             
         {
@@ -154,136 +153,28 @@ check.snowball.conflict <- function(snowball, force.install, ignore.deps, date) 
 
           #Assess if any packages in conflict are always locally available suggesting came from local library
                 original_lib_path <- .pkgenv[["orig_lib_paths"]]
-                install.packages_current <- data.frame(utils::installed.packages(noCache = FALSE, lib.loc = original_lib_path), stringsAsFactors = FALSE)
-                installed.pkg_vrs <- paste0(install.packages_current$Package,"_",install.packages_current$Version)
+                installed.packages_current <- data.frame(utils::installed.packages(noCache = FALSE, lib.loc = original_lib_path), stringsAsFactors = FALSE)
+                installed.pkg_vrs <- paste0(installed.packages_current$Package,"_",installed.packages_current$Version)
                 remove.set <- (installed.pkg_vrs %in% conflict.active)
                 
-                
-          #General introduction to the problem
-            msg.repeat.conflict.header <-paste0(
-                  "\n\n",
-                  "YOU RECENTLY GOT THIS MESSAGE, SO A BIT MORE INFO:\n",
-                  "If you get this message even after restarting the R session, the packages(s) generating\n",
-                  "the conflict are being called on automatically. The most common scenario is R Studio\n",
-                  "loading the packages behind the scenes before you run the groundhog.library() command.\n",
-                  "For instance, R Studio often loads packages referred elsewhere in a script with the\n",
-                  "<pkg>:: operator (e.g., load 'dplyr' if it finds dplyr::filter() ).\n",
-                  "R Studio also loads packages when it works on .rmd files (e.g., 'knitr' & 'xfun).\n\n",
-                  "Less common scenarios involve packages you loaded earlier loading a dependency \n",
-                  "without properly documenting it, having in your environment objects that require \n",
-                  "those packages, and modifications to the .rprofile file, which load some packages \n",
-                  "by default when you start R.\n\n",
-                  "OK. That's the problem. Here are your possible *solution(s)* :"
-                  )
-            
-            
-              
-          #1. New order
-            msg.repeat.conflict.explicit.first <- paste0(
-                  #Option 1 
-                  "- Solution for the less common case of improperly documented packages: Use groundhog to \n",
-                  "  explicitly load the conflicting dependency first e.g., run:\n", 
-                  "  groundhog.library('",dep.example, "','" , date , "')  before groundhog.library('", requested_pkg , "','" , date , "').\n"
-                  )
-                    
-          #2. Ignore
-                  msg.repeat.conflict.ignore <- paste0(
-                  "- Workaround for all cases (this compromises version control): Tell groundhog to ignore this\n",
-                  "  particular conflict and continue loading '",requested_pkg, "', by using the 'ignore.deps' argument.\n",
-                  "  For example: groundhog.library('" ,requested_pkg , "','" , date , "', ignore.deps='",dep.example,"')\n"
-                  )
-            
-          #3. Uninstall
-                  msg.repeat.conflict.uninstall <- paste0(
-                  "- Solution for all cases: Uninstall the conflicting package(s) from your non-groundhog library,\n",
-                  "  ensuring they can't be loaded by R or R Studio. Groundhog can do this easily for you, just type \n",
-                  "  'uninstall' below. All conflicting package(s), and all packages that depend on them, if any, \n",
-                  "  will be uninstalled from your non-groundhog library.\n",
-                  "  You can undo this action and reinstall them at any time, even months from today, by running\n",
-                  "  'groundhog::reinstall.conflicts()'.\n",
-                  "  But if you use groundhog to manage your packages, you shouldn't need to.\n\n"
-                  )
-            
-                  
-          #Print options
-          #0) If at least one of them applies, show header
-                if ((!requested_pkg %in% conflict.active.pkg)  | (sum(remove.set)>0) )  {
-                  message(msg.repeat.conflict.header)
+        # If at least one of them can be removed
+                if (sum(remove.set)>0 )  {
+                   message1("It's likely the packages creating the conflicts are being loaded automatically.\n",
+                          "\n-->  A simple solution: run  `disable.local()`\n",
+                				  "\n  This will disable all packages in your local (non-groundhog) library.\n"  ,
+                				  "  If at any time you want to go back to using those packages (e.g., using `library()`),\n",
+                				  "  you just run `enable.local()` and all packages will instantaneously be available again.\n\n",
+                				  "  For other solutions see https://groundohgr.com/conflicts")
+                  message("\nAgain, the simple solution is `disable.local()`")
                 }
-                  
-                  
-          #1) Unless the wanted package is the conflict, suggest putting it first or ignoring deps
-                  if (!requested_pkg %in% conflict.active.pkg) {
-                      message(msg.repeat.conflict.explicit.first)
-                      message(msg.repeat.conflict.ignore)
-                      }
-                  
-          #2) If the package is removable, suggest removing it 
-               if (sum(remove.set)>0)  
-                  {
-                  message(msg.repeat.conflict.uninstall)
                       
-                  #While loop for readline to avoid submitted code to be interpreted as the answer
-        						text <-''
-        						j <- 1 #counter of times message is shown
-        						while (text!="uninstall" & text !="keep")  
-        						{
-        						text <- readline(prompt = "To unistall conflicting packages type 'uninstall', to keep them type 'keep' >")
-        						text <- strip.prompt(text)
-        						if (text !="uninstall" & text != "keep") {
-        						message(j , ") You answered: '", text , "'") 
-        						message("   To ensure you are actively answering, only 'uninstall' and 'keep' are accepted as responses\n")
-        						j<-j+1 #Add to counter of msgs rejected
-        						} #end if
-                    } #End while
-									
-					
-					#If they typed uninstall, do it
-                  if (text=="uninstall") {
-                    remove.conflict(conflict.active)
-                  } 
-                      
-           #if they typed keep
-                  if (text == "keep") {
-					          message("You typed 'keep', will NOT uninstall conflicts.")
-					        }
-					exit()
-               }
-        }
-      
            
-  #9 Warning about ignored conflicts, if all conflicts were ignored
-       #9.1 Find Packages with a conflict that is being ignored
-            conflict.ignored    <- snowball$pkg_vrs[!(snowball$pkg_vrs %in% active$pkg_vrs) & (snowball$pkg %in% active$pkg) & (snowball$pkg  %in% ignore.deps_default())]
-            conflict.ignored.pkg <- snowball$pkg    [!(snowball$pkg_vrs %in% active$pkg_vrs) & (snowball$pkg %in% active$pkg) & (snowball$pkg %in% ignore.deps_default())]
-          
-       #9.2 If any, describe them
-            
-            if (length(conflict.ignored) == length(conflict.active))
-              {
-              message("warning")
-              message2("groundhog.says: version mismatch")
-              message1(
-                      "A different version of at least one of the needed packages is already loaded, \n",
-                      "this usually would terminate the groundhog.library() call and you would be asked\n",
-                      "to restart the R session, but the package(s) involved:\n",
-                      "(" , conflict.ignored.pkg , ")", " are in the set for which conflicts are tolerated either because\n",
-                      "they are often loaded automatically (e.g., by R Studio) from your local R library, and/or are \n",
-                      "'recommended' packages by R and thus version control can be attained by using the version of R matching\n",
-                      "the date you entered, or you previously loaded those a version of those packages from a remote\n",
-                      "repository (e.g. github).\n\n",
-                      "You may want to try a session restart (in R Studio: SHIFT-CTRL-F10) to remove the conflict and \n",
-                      "enable loading the desired version but the problem may persist.\n",
-                      "At that point you may either tolerate imperfect version control or resolve the issue by changing how \n",
-                      "you are running the script. For example, if 'knitting', you could  bypass R Studio automatically loading\n",
-                      "packages by writing the script as an .R file and using rmarkdown::render() instead of the point-and-click\n",
-                      "'knit' button solution offered by R Studio. While this issue is most common when using R Studio for .rmd files, \n",
-                      "it may arise in other scenarios."
-                      )
-              }
+
              
+
+     } #End if last_conflict<5
   #9 Return
      invisible(list(packages.needed=conflict.needed, packages.active=conflict.active))
      exit()
-  } # End if some conflict found
+  }# End if some conflict found
 } # End function
