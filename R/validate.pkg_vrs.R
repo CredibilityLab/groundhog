eb6e3c62a2ff02d661f95666c294290590501c2c
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2022-05-14 04:09:07 +0200

    Update validate.pkg_vrs.R

--- R/validate.pkg_vrs.R
+++ R/validate.pkg_vrs.R
@@ -13,10 +13,15 @@
     
   #1 No 'groundhog'
         if ("groundhog" == pkg) {
-        message("groundhog says: Error. You may not use groundhog.library() to load groundhog.\n",
-                "To load the version of groundhog available on '", date, "', please use:\n",
-                "meta.groundhog('" ,  date , "')"
+        txt <- paste0(
+                "|IMPORTANT\n",
+                "|     groundhog says: You may not use groundhog.library() to load groundhog.\n",
+                "|     To load the version of groundhog available on '", date, "', you may run:\n",
+                "|     meta.groundhog('" ,  date , "')",
+                "|     Type OK to confirm you have read this message."
                  )
+        
+        answer <- infinite.prompt(txt,'ok')
         exit()
         } #End if groundhog
 
@@ -35,7 +40,7 @@
         
   #3 Early return if already attached
             if (pkg_vrs %in% attached.pkg_vrs) {
-                  message1("The package '", pkg_vrs, "' was already attached.")
+                  message1("The package '", pkg_vrs, "' is already attached.")
                   return('already_attached')  
             }
                 
@@ -43,78 +48,37 @@
               #but if more where submitted in pkg=c(pkg1,pkg2) it will just move on to the next package
               #instead of ending the entire groundhog.library() call
       
-  #4 CONFLICT
-             
-   #4.1 Mismatched package already attached  
-         if ((pkg %in% attached.pkg) &  (!pkg_vrs %in% attached.pkg_vrs)) {
-            message1(
-                    "Another version of '", pkg,"' is already attached ('", active$pkg_vrs[active$pkg==pkg],"').\n",
-                    "To solve this: restart the R session. Note: you will need to do 'library(groundhog)' again.\n\n",
-                    "In R Studio press: CTRL/CMD-SHIFT-F10"
-                    )
+  #4 Another version already attached
+        #Add message if dates mismatch across groundhog library calls
+          if ((pkg %in% attached.pkg) &  (!pkg_vrs %in% attached.pkg_vrs)) {
+            txt <- paste0(
+                    "|IMPORTANT\n",
+                    "|    Groundhog says: another version of '", pkg,"' is already attached ('", active$pkg_vrs[active$pkg==pkg],"').\n"
+                      )
            
            
         #Add message if dates mismatch across groundhog library calls
          if (length(.pkgenv[['hogdays']])>1) {
-            message("groundhog says: Warning! You have used different groundhog days (dates) across groundhog.library() calls, \n",
-                    "this may be causing the conflict of versions.") 
-            message("\nDates you have used: ",paste0(.pkgenv[['hogdays']],collapse=' , '))
-            message("Check your script and use a single groundhog day across all calls.")
+            txt <- paste0(txt, 
+                    "|    In this R session, you have used different dates to load packages.\n",
+                    "|    Dates you have used: ",pasteQC(.pkgenv[['hogdays']]), "\n",
+                    "|    If possible use the same date for all packages\n"
+                  ) #End of paste0
+            
+            
           }
            
+          txt<-paste0(txt,
+                      "|    You may restart the R session to unload all packages.\n",
+                      "|    (In R Studio CMD/CTRL-SHFT-F10)\n\n",
+                      "|    Type 'OK' to confirm you have read this message.")
            
-          message("\nThe package '", pkg_vrs,"' was *NOT* attached")
+          infinite.prompt(txt,'ok')
           exit()
          }
         
     
-    #4.2 Attach mismatched version if ignore.deps is loaded but not attached (common scenario, trying to attach knitr in .rmd file)
-       if ((pkg %in% active$pkg) &                         #pkg matches
-           (!pkg_vrs %in%  active$pkg_vrs) & 
-           (pkg %in% ignore.deps))                          #in ignoreable conflict list.
-        {
-           
-        #Attach it 
-		     base.library(pkg, character.only=TRUE, lib.loc=c(.libPaths(), .pkgenv[["orig_lib_paths"]])) #utils.R function 27
 
-             
-        #Make local variable with name of pkg_vrs already loaded oralready in libpath creating conflict
-             #available.pkg_vrs <- ifelse(pkg  %in% active$pkg, active$pkg_vrs[active$pkg==pkg] , paths.pkg_vrs[paths.pkg==pkg])
-             
-        #Message  
-          message1("groundhog says: succesfully attached '" , pkg , "'")
-          message("\ngroundhog says: Warning!\n", 
-                   "'", pkg, "' was already loaded and it is now attached,\n",
-                  "BUT the version previously available and just attached ('" ,active$pkg_vrs[active$pkg==pkg] , "')\n",
-                  "does not match the version for '" , date, "' ('", pkg_vrs , "').\n",
-                  "To attach the desired version you can try restarting the R session.\n\n",
-                  "In R Studio press: CTRL/CMD-SHIFT-F10\n\n",
-                  "Note that it is possible that this package is being loaded automatically from your local\n",
-                  "library (e.g., by R Studio). In this case, after restarting the session the problem will persist.\n",
-                  "You can ignore this problem and tolerate lack of version control for the involved packages.\n", 
-        				  "You can also prevent this problem by uninstalling the package from your non-groundhog library\n",
-        				  "running: remove.packages('", pkg ,"'), but if R Studio is using the package (e.g., the\n",
-        				  "'knit' button requires 'knitr' outside of groundhog), you may need to rely on R rather than\n",
-        				  "R Studio for that disabled functionality."
-        				 )
-		
-          
-        #Explain the recommended pkgs to highlight this may be trickier 
-             ip <- data.frame(utils::installed.packages(),stringsAsFactors = FALSE)
-             recommended.pkgs <- unique(subset(ip, ip$Priority=="recommended")$Package) 
-             
-               #Note:unique because there may be two versions of the same package in different libraries
-    
-            if (pkg %in% recommended.pkgs) {
-              message(
-                    "The package in question '", pkg , "', is a 'recommended' package which makes removing it from the\n",
-                    "local (non-groundhog) library potentially more problematic; this is why this conflict\n",
-                    "is tolerated by groundhog, producing a warning rather than an error."
-                    )
-            }
-          exit()
-
-        }   
         return('')
         
     } #End function
