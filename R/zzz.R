d91264c5a093a34080cc542a8369e24dda9ec3d7
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2022-07-10 07:54:13 +0200

    trim zzz

--- R/zzz.R
+++ R/zzz.R
@@ -44,15 +44,17 @@
 
     
 #2. Attaching 
-    #2.1 Show message of library path 
     #' @importFrom utils packageVersion compareVersion
+     
     .onAttach <- function(libname, pkgname) {
       
-      #Consent previously?
+      
+      
+      #1. Check if consent to write to local folder used for groundhog files has been given
         main_folder <-  paste0(path.expand("~"), "/R_groundhog")
         consent <- (file.exists(main_folder))
         
-       #Check if folder exists, or ask for consent otherwise
+      #2. If no consent, ask for it
          if (consent == FALSE) {
             message("groundhog needs authorization to save files to  '",main_folder, "'\n",
                                   "Enter 'OK' to provide authorization")
@@ -65,68 +67,38 @@
               }
               } #End if consent == FALSE
         
-      # If No consent, die
+      #3. It not given, die
           if (consent == FALSE)
           {
           message("You did not say 'OK'\nIf you run 'groundhog.library()' you will be asked again")
           }
         
-      #Proceed only if consent exists
+      #4. If consent given, proceed
           if (consent == TRUE)
           {
               #Create the folder (when running groundhog.library() this will signal consent was given)
                 dir.create(main_folder, showWarnings = FALSE, recursive = TRUE)
-               
-                
-      #Load toc files
+        
+            
+      #5 Load toc files
           load.cran.toc()    
 
-        #Report versions being used
+      #6 Report versions being used
           groundhog.version_using <- as.character(packageVersion("groundhog"))
           r.using.full= get.rversion() 
           packageStartupMessage ("Loaded 'groundhog' (version:",packageVersion('groundhog'),  ") using R-" ,r.using.full) 
           packageStartupMessage ("Tips and troubleshooting: https://groundhogR.com")
         
            
-    #2.2 check for update
-    # isTRUE() is necessary here because this will return logical(0) if the pkg
-    # is not on CRAN, or if working offline (current.packages is NULL in this case).
-      #Try to read from groundhogr.com   
-          groundhog.version_cran <- tryCatch(
-          as.character(readLines("https://groundhogr.com/groundhog_version.txt")),
-          warning = function(w) NULL,
-          error = function(e) NULL
-        )
-       
-    #Get majmin
-      if (!is.null(groundhog.version_cran)) {
-          gv.using <- as.numeric(strsplit(groundhog.version_using, "\\.")[[1]])
-          gv.cran  <- as.numeric(strsplit(groundhog.version_cran, "\\.")[[1]])
-          gv.using.majmin <- 10000*gv.using[1] + gv.using[2]
-          gv.cran.majmin <-  10000*gv.cran[1]  + gv.cran[2]
-    
-    
-        if (isTRUE(gv.cran.majmin > gv.using.majmin)) {
-          packageStartupMessage(
-            "\n\n\n",
-            "          OUTDATED GROUNDHOG\n",
-            "            You are using version  '" , groundhog.version_using, "\n",
-            "            The current version is '" , groundhog.version_cran, "'\n\n",
-            "            You can read about the changes here: https://groundhogr.com/changelog\n\n",
-            "Please update by running: \ninstall.packages('groundhog')"
-            )
-            }  #End mismatch in version
-          } #End if !is.null()
-          } #ENd if consent==TRUE
+      #7 check for need to update groundhog
+        check.groundhog.version(min.days=0) #Function 42  -  utils.R
         
-        
-        
-     #Verify a mirror has been set    
-      set.default.mirror() #Function 36
+      #8 Verify a mirror has been set    
+        set.default.mirror() #Function 36 -  utils.R
 
  
-     
-      } #End on attach
+      } #End if consent==TRUE
+    } #End on attach
     
     
       
