db99c1bd8f829f9e239d66a93107dbd2756fb62b
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2022-07-26 08:46:31 +0200

    removed hardcoded repos link

--- R/get.current.packages.R
+++ R/get.current.packages.R
@@ -20,9 +20,7 @@ get.current.packages <- function(type) {
         } else {
   #Else get available packages to see if each attempted to install is new
       current.packages <- tryCatch({
-          as.data.frame(
-          utils::available.packages(contriburl = utils::contrib.url(repos = "https://cloud.r-project.org/", type = type))[, c(1, 2)],
-          stringsAsFactors = FALSE
+          as.data.frame(utils::available.packages(type = type)[, c(1, 2)],stringsAsFactors = FALSE)
         )},
         error = function(e) NULL)
 		
