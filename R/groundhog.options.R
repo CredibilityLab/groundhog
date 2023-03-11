0e5992ac9d1a30bd1fb5aa41831842a718f711f8
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2023-03-11 07:30:08 +0100

    groundhog options

--- R/groundhog.options.R
+++ R/groundhog.options.R
@@ -1,26 +1,52 @@
+#' Set an option for groundhog
+#
+#' @param name Character. The name of the option being set.
+#' @param value Character or logical. The value to assign the option to.
+#' 
+#' @details
+#' Current options that can be set manually include the following:
+#' 
+#' [os]: operating system. It is supposed to be detected automatically, but if
+#' groundhog were to not correctly identify a user's 'os' it can be set manually.
+#' Accepted options are 'windows','mac', 'mac_arm' (for the x1+ chip), 'linux'.
+#'  
+#' [download.sequentially]. To speed up the download of binaries, they are 
+#' by default downloaded with 'libcurl' simultaneously, if 'libcurl' is detected.
+#' A user can manually turn this off to avoid possible errors. This will lead to
+#' downloading packages one a time, slowing down installations by a few seconds per 
+#' package (& dependency).
+#' @examples
+#' \dontrun{
+#' set.groundhog.option('download.sequentially',TRUE)
+#' }
+#'
+#' @export
+#'
+#' @seealso [get.groundhog.option()]
+#'
+#'
 
-
+#Function 1 set it
   set.groundhog.option <- function(name, value)
   {
 
-  #1 Validate accepted options
-    accepted.names <- c('os')
-    
-    #If not accepted error
-      if (!name %in% accepted.names) {
-         stop("groundhogg says: '",name,"' is not one of the accepted options. ",
-         "\nThe set of accepted options is:\n",pasteQC(accepted.names))
-      }
+  #1   #If not accepted error
+       #Set in zzz.R
+          if (!name %in% .pkgenv[['acceptable.option.names']]) {                     
+             stop("groundhogg says: '",name,"' is not one of the accepted options. ",
+             "\nThe set of accepted options is:\n",pasteQC(accepted.names))
+            }
     
   #------------------------    
   #2 Possible values
       accepted.values <- list()
     
-    #2,1 OS
+    #2.1 OS
         accepted.values[['os']] <-c('windows','mac','mac_arm','linux')
         
-    #2.2 NEXT OPTION - TBA
-        
+    #2.2 Skip libcurl in download
+        accepted.values[['download.sequentially']] <- c(TRUE,FALSE)
+
   #----------------------------
         
   #3 Validate values
@@ -33,12 +59,14 @@
       
   #----------------------------     
   #4 store it
+        
     #Set it in environemnt
       .pkgenv[[name]] <- value
     
     #Save it to path
       path <-  paste0(path.expand("~"), "/R_groundhog/options/",name,".txt")
-      write(path,value)
+      dir.create(dirname(path), showWarnings = FALSE,recursive = TRUE)
+      write (value,path)
       
     #Message
       message1("The value for '",os,"' has been set to '",value,"'")
@@ -46,11 +74,26 @@
     } #End of function
 
   
+#-----------------------------------------------------
+  
+#' Get groundhog option
+#' 
+#' Obtain the value currently assigned to a manually set option.
+#'
+#' @examples
+#' \dontrun{
+#' get.groundhog.option("download.sequentially")
+#' }
+#'
+#' @export
+#'
+#' @seealso [set.groundhog.option()]
+#'
   
   get.groundhog.option <- function(name)
   {
    #1 Validate accepted options
-    accepted.names <- c('os')
+    accepted.names <- c('os','download.sequentially')
     
     #If not accepted error
       if (!name %in% accepted.names) {
@@ -69,4 +112,3 @@
   } #End of get.groundhog.option()
   
   
-
