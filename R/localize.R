f0a8f9b66bc72177608fea389e34bc77c9efc42d
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2023-04-04 19:44:12 +0200

    localize saves to backup only if needed

--- R/localize.R
+++ R/localize.R
@@ -8,104 +8,144 @@
     
     #0.5 Restore point: save dataframe with installed packages with today's date if not yet saved, for possible restore late
 
-      ip_path <- paste0(get.groundhog.folder(),"/restore_points/", get.r.majmin(), "/",Sys.Date(),".rds")
-      if (!file.exists(ip_path)) {
+      restore_path <- paste0(get.groundhog.folder(),"/restore_points/", get.r.majmin(), "/",Sys.Date(),".rds")
+      if (!file.exists(restore_path)) {
         
         #Create directory for IP  
-          dir.create(dirname(ip_path),showWarnings = FALSE,recursive = TRUE)
+          dir.create(dirname(restore_path),showWarnings = FALSE,recursive = TRUE)
         #Get IP
           ip <- data.frame(installed.packages( .pkgenv[["orig_lib_paths"]][-length(.pkgenv[["orig_lib_paths"]])]),row.names = NULL,stringsAsFactors = FALSE)
         #Drop base pkgs
           ip <- ip[!ip$Package %in% base_pkg(), ]
+      
+        #Drop possible duplicates (if multiple paths exists with the same package, we will work with the first one, and replace that one)
+          ip<-ip[! duplicated(ip$Package),]
+          
         #Get pkg_vrs
           ip$pkg_vrs <- paste0(ip$Package,"_",ip$Version)
         #Keep only lib and pkg_vrs
           ip <- data.frame(LibPath=ip$LibPath, pkg_vrs=ip$pkg_vrs)
+          
         #Save it
-          saveRDS(ip , ip_path)
+          saveRDS(ip , restore_path)
       }
     
-    #0.5 Vector with purged 
-        purged.pkg_vrs = c()
         
-    #1 Load installed packages
-        ip <- data.frame(utils::installed.packages(lib.loc =.pkgenv[["orig_lib_paths"]][1] ), stringsAsFactors=FALSE, row.names=NULL)
+  #--------------------------------------------------------------------------               
         
-      #If none, create empty row to avoid errors when combining pkg_vrs
-        if (nrow(ip)==0) {
-        ip[1,] <- rep('',ncol(ip))
-        }
         
-    #2 Make pkg_vrs
-        ip$pkg_vrs<-paste0(ip$Package,"_",ip$Version)
+    #1  Installed packages: local, groundohg and backup
+       
+      #1.1 Locally
+         ip <- data.frame(utils::installed.packages(lib.loc =.pkgenv[["orig_lib_paths"]][1] ), stringsAsFactors=FALSE, row.names=NULL)
+        
+        #If none, create empty row to avoid errors when combining pkg_vrs
+          if (nrow(ip)==0) ip[1,] <- rep('',ncol(ip)) 
+        
+        #pkg_vrs 
+          ip$pkg_vrs<-paste0(ip$Package,"_",ip$Version)
+          
+          
+    ##############################################################################          
+    #EARLY RETURN IF ALL LOCAL ALREADY
+          if (all(snowball$pkg_vrs %in% ip$pkg_vrs)) return(invisible(TRUE))
+
+    ##############################################################################
+                
+    #1.2 In Groundhog 
+         #Master path   
+             groundhog.master_path <- paste0(get.groundhog.folder() , "/R-" , get.r.majmin())
       
+         #All pkgs in that path    
+            ip.groundhog <- data.frame(utils::installed.packages(list.files((groundhog.master_path),full.names = TRUE)),
+                                       row.names = NULL, stringsAsFactors = FALSE)
+            
+         #If none, create empty row to avoid errors when combining pkg_vrs
+          if (nrow(ip.groundhog)==0) ip.groundhog[1,] <- rep('',ncol(ip.groundhog))
+            
+            
+        #pkg_vrs
+            ip.groundhog$pkg_vrs = paste0(ip.groundhog$Package,"_",ip.groundhog$Version)
+        
+         
+            
+    #1.3 Backup path
+            backup.dir <- paste0(get.groundhog.folder(),"/restore_library/" , get.r.majmin() , "/")
+            ip.backup <- data.frame(utils::installed.packages(list.files(backup.dir, full.names = TRUE)),
+                                    row.names = NULL, stringsAsFactors = FALSE)
+            if (nrow(ip.backup)==0) ip.backup[1,] <- rep('',ncol(ip.backup))
+            ip.backup$pkg_vrs <- paste0(ip.backup$Package , "_" , ip.backup$vrs)
+             
     
-    #3 If entire snowball is not remote, assign sha='' to snowball
+            
+               
+              #
+  #--------------------------------------------------------------------------               
+                 
+                 
+   #2 If entire snowball is not remote, assign sha='' to snowball
       if (!'sha' %in% names(snowball)) snowball$sha=''
     
 
- #4 Start the loop over the snowball
-  for (k in 1:nrow(snowball))
-    {
-    #5 Short varnames
-      pkg     <- snowball$pkg[k]
-      pkg_vrs <- snowball$pkg_vrs[k]
-      installation.path <- snowball$installation.path[k]
-      sha <- snowball$sha[k]
+# Start the loop over the snowball  #k=6
+        
+for (k in 1:nrow(snowball))
+      {
+  
+      #3 Short varnames
+        pkg     <- snowball$pkg[k]
+        pkg_vrs <- snowball$pkg_vrs[k]
+        installation.path <- snowball$installation.path[k]
+        sha <- snowball$sha[k]
          
-    #6 If localized already, skip  
-      if (pkg_vrs %in% .pkgenv[['localized']]) next
-
-    #7 If local folder already has this version and the pkg in snowball is not remote
+    #4 SKIP if already local 
       if (pkg_vrs  %in% ip$pkg_vrs & sha %in% c('', NA)) next
       
         
-    #8 If package does not exist in groundhog folder, error    
+    #5 If package does not exist in groundhog folder, error    
         if (nrow(data.frame(utils::installed.packages(lib=installation.path), stringsAsFactors=FALSE,row.names=NULL))==0) {
           msg = paste0("groundhog says: failed to install '",pkg_vrs,"', localization failed (Error: localize.R #8 - try http://groundhogr.com/troubleshoot)")
           gstop(msg) #util #51
         }
 
-    #9 all paths except the last one
-         local_folder <- .pkgenv[["orig_lib_paths"]][-length(.pkgenv[["orig_lib_paths"]])]
       
-    #10 If this one is installed, set it ready to be purged next time we load groundhog and make copy
-        if (pkg %in% ip$Package) {
+    #6 PURGE: With conflit
+    #  If different version of this pkg is already local, purge and ensure backup exists
+         
+         #Same pkg, diferent vrs
+          if (pkg %in% ip$Package &  !(pkg_vrs %in% ip$pkg_vrs)) 
+            {
           
-            #10.1 Copy to groundhog folder if it does not exist 
-          
-             #File paths
-               non.groundhog.path <- file.path(ip$LibPath[ip$Package==pkg] , pkg) 
-               groundhog.path     <- paste0 (dirname(installation.path), "/", ip$pkg_vrs[ip$Package==pkg] , "/")
-               exists <- file.exists(groundhog.path) && nrow(utils::installed.packages(groundhog.path))>0
-              
-                  
-            #10.2 If it does not exist and it is not remote, create and copy
-               if (exists==FALSE  & sha %in% c('', NA)) {
-
-                #Create
-                  dir.create(groundhog.path,recursive = TRUE, showWarnings = FALSE)
-
-                #Copy non-remote to groundhog.folder
-                  copy.outcome <- file.copy(non.groundhog.path ,        #copy contents of the "pkg_vrs/pkg" folder
-                              groundhog.path,           #to the local library listed first
-                              recursive = TRUE)          #include all files
-
-              } #End 10.2
-               
-               
-            #10.3 to the purge list 
-              old<- file.path(ip$LibPath[ip$Package==pkg] , pkg) 
-              random <- paste0(sample(letters,size=6),collapse = '')
-              new <- paste0(old , "_",random,"_PURGE")  #add 6 random letters and _PURGE
-              purged   <- file.rename(old , new)
-              
-           
+          #6.1 Get pkg_version in local folder now  
+             pkg_vrs.existing <- ip$pkg_vrs[ip$Package==pkg]
             
-        } #End if this pkg we are installing is already installed in ip so we need to purge
+          #6.2 If this pkg_vrs is neither in groundhog nor backup, save it to backup (if it was installed with groundhog it would be in groundhog) 
+                 if (!(pkg_vrs.existing %in% ip.groundhog$pkg_vrs) &  #It is not in groundhog
+                     !(pkg_vrs.existing %in% ip.backup$pkg_vrs))       #it is not in backup) 
+                {
+            
+                #Copy to groundhog.folder
+                 local.pkg_path <- file.path(ip$LibPath[ip$Package==pkg] , pkg) 
+                 backup.pkg_path <- paste0(backup.dir, pkg_vrs.existing)
+                 dir.create(backup.pkg_path, recursive = TRUE,showWarnings = FALSE)
+                  copy.outcome <- file.copy(local.pkg_path , #copy contents from personal folder
+                                            backup.pkg_path,                       
+                                            recursive = TRUE)   #include all files
+
+                } #End 6.2
+               
+       #6.3  Rename to "_#####_PURGE" 
+            old<- file.path(ip$LibPath[ip$Package==pkg] , pkg) 
+            random <- paste0(sample(letters,size=6),collapse = '')
+            new <- paste0(old , "_",random,"_PURGE")  #add 6 random letters and _PURGE
+            purged   <- file.rename(old , new)
+              
+    } #End #6 - if conflict 
          
-         
-    #11  Copy the folder from groundhog folder
+     
+#-------------------------------------------------------------
+      
+    #7  Copy the folder from groundhog folder
          #path to copy pkg from and to
             from_path <-paste0(installation.path,'/',pkg)  #groundhog_folder
             to_path <-  paste0(local_folder[1])            #local_folder
@@ -124,13 +164,12 @@
           message("groundhog says: failed to copy '", pkg_vrs,"' to default personal library")
         }
         
-    #12 Show feedback 
+ #-------------------------------------------------------------   
+        
+    #8 Show feedback 
       if (localize.quietly==FALSE & copy.outcome==TRUE) message1("  copying: ",pkg_vrs)
      
-        
-    #13 Add to localized  vector
-        .pkgenv[['localized']]<-c(.pkgenv[['localized']], pkg_vrs)
-         
+   
     } #End snowball loop
         
 } #End localize function
