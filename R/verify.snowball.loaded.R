51bd082aa2bdfdf03c9ec9e0da2abb53396cdab6
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2023-04-07 08:18:29 +0200

    previously attached don't get msg

--- R/verify.snowball.loaded.R
+++ R/verify.snowball.loaded.R
@@ -34,8 +34,8 @@
                       gstop(msg)
                       }
        
-        #2.3 Successfully attached pkg_vrs
-            if (pkg_vrs %in% attached$pkg_vrs)
+        #2.3 Successfully attached pkg_vrs (if it is attached now, and was not attached before
+            if (pkg_vrs %in% attached$pkg_vrs & !pkg_vrs %in% .pkgenv[['attached']]$pkg_vrs) #See groundhog.library #5
               {
               message1("Succesfully attached '", pkg_vrs,"'")
               } 
