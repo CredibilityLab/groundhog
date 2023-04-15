8479e1699d442cfd8033b76406d31fd18a9934fc
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2023-04-15 10:26:23 +0200

    debugging restore library

--- R/restore.library.R
+++ R/restore.library.R
@@ -1,14 +1,14 @@
 #' Restore version of packages in personal library replaced by groundhog.
 #'
-#' Instantaneously reverse installations and removals of packages in the 
-#' default personal library (`libPaths()[1]`) made by groundhog, when installing different versions
-#' of those packages.
+#' Instantaneously reverse changes made by groundhog to the default personal 
+#' library (`.libPaths()[1]`).
 #' 
 #'@param days an optional numeric argument used to choose among alternative restore points.
 #' When `days` is set, groundhog restores the personal library to the  most recent restore point, that 
-#' is at least `days` days ago. For example, if there are two restore points: one from today, and one from 7 days ago,
-#' setting `days=3` would restore to the latter, and setting `days=8` would result in an error. 
-#' `days = -1` will restore back to the oldest restore point available.
+#' is at least `days` days ago. If it is not set it restores to the most recent restore point. 
+#' For example, if there are two restore points: one from today, and one from 7 days ago,
+#' running `restore.library()` would restore to the former, setting `days=3` would restore to the latter, 
+#' and setting `days=8` would result in an error. `days = -1` restores to the oldest restore point available.
 
 
 #' @examples
@@ -21,7 +21,7 @@
 #'
 #' @details 
 #'When groundhog installs a package, it installs it into groundhog's library. 
-#'To find the location of this folder: `get.groundhog.folder()`.
+#'To find the location of this folder: [get.groundhog.folder()].
 #'Groundhog then immediately moves the installed package (and its dependencies) 
 #'to the default personal library (to find its location: `.libPaths()[1]`). 
 #'Altering the packages in this folder is important for two main reasons. First,
@@ -34,17 +34,18 @@
 #'any existing other versions of those packages out, to another directory (a local archive).
 #'Those files are not deleted, just moved, making it easy and fast to recover.
 #'When the first change in the personal folder is made on a given calendar date, 
-#'Groundhog makes a list of all packages available in the personal folder before such change,
-#'creating a 'restore point'. With `restore.library()` groundhog looks up that set of packages
-#'removes any packages in the personal library which were not in that restore points, and 
-#'moves back any packages that were available but are not available now. This process will
+#'groundhog makes a list of all packages available in the personal folder before such change (saving
+#'a copy of the results from `installed.packages(.libPaths()[1])`), this saved file is referred to as a 
+#'a 'restore point'. With `restore.library()` groundhog looks up a restore point, obtain the set of packages
+#'that used to be installed, and removes any packages in the personal library which were not in that restore point, 
+#'and moves back any packages that no longer are This process will
 #'typically take substantially less than 10 seconds. Note that there is only one restore point
-#'per calendar date, so after using `groundhog.library` on a given day, so one effectively restores
+#'per calendar date, so one effectively restores
 #'the personal library to how it was before *any* changes were made to it that day with groundhog. 
 #'Restore points are saved permanently and can be restored at any point. 
-#'To view saved restore dates view the hidden dataframe `.available.restore.points`. 
+#'The set of restore points available is stored in the hidden dataframe `.available.restore.points`. 
 #'To choose among them use the `days` argument in `restore.library`. The default is to restore 
-#'based on the most recent restore points, so if a user installs groundhog, tests it, and wants to 
+#'based on the most recent restore point, so if a user installs groundhog, tests it, and wants to 
 #'undo all changes made by groundhog, the default behavior will achieve this goal.
 
 #' @export
@@ -114,6 +115,7 @@ restore.library<-function(days)
   #6 Inform user what would happend if they go forward
       
       #6.0 Restore back to ONLY date
+        msg=''
        if (missing(days)) 
         {
         msg <- paste0(
@@ -165,39 +167,34 @@ restore.library<-function(days)
            
           #From/To based on ip.add (recall: ip.add is a subset of ip.restore)
             from <-paste0(backup.dir, ip.add$pkg_vrs)
-            to   <- ip.add$LibPath
+            to   <- paste0(ip.add$LibPath, "/", get.pkg(ip.add$pkg))
             
           #Skip if from does not exist skip
             skip.no_backup <- !file.exists(from)
-            skip.other_pkg <- file.exists(to)
+            skip.other_pkg <- file.exists(paste0(to,"/", ip.add$pkg))
             skip <- skip.no_backup | skip.other_pkg
           
           #Move 
             outcome <- file.rename(from[!skip] , to[!skip]) 
             
-          #Message
-            #All success
-              if (sum(outcome)==n.add) message1("Restored ",n.add, " packages.")
           
-            #Skipped based on FROM
-              if (sum(skip.no_backup)>0) {
-                message("An additional ",sum(skip.no_backup), " other packages packages could not be restored due to missing backup:")
-                message(pasteQC(from[skip.no_backup]))
-              } 
-          
-          #Skipped based on TO
-              if (sum(skip.other_pkg)>0) {
-                message("An additional ",sum(skip.other_pkg), " other packages packages could not be restored because they \n",
-                        "were already available in the destination library (and were not installed with groundhog):")
-                message(pasteQC(from[skip.other_pkg]))
-              } 
-          } 
+            } 
   
     #------------------------------------------------
             
+  #9 Compare obtained with expected
+        ip.now<-get.ip('local')
+      
+        n.total    <- nrow(ip.restore)
+        n.restored <- sum(ip.restore$pkg_vrs %in% ip.now$pkg_vrs)
+    
+  
   #10 Message
         msg = paste0("Library restoration completed.\n",
+                     n.restored, " out of ", n.total," packages restored.\n",
                      "Restart the R session to proceed.")
+      
+      #Verify
         infinite.prompt(msg,valid_answers='uncle',must.restart = TRUE)
             
       } #IF they answered 'restore
