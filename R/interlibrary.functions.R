4ca027ccdfc82b6b8d5cc666f019eb68ea11b465
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2023-04-16 23:53:24 +0200

    add /pkg to bakcups (bug fix)

--- R/interlibrary.functions.R
+++ R/interlibrary.functions.R
@@ -3,7 +3,7 @@
 
 #Function 1 - Fed the subset of an IP that needs to be purged, and based on a loans[] dataframe it returns loans or backups the pkgs
 
-  purge.local <- function (ip.purge , loans)
+purge.local <- function (ip.purge , loans)
   {
     
   #1 Allocate to be purged packages to return to groundhog vs to back up 
@@ -21,7 +21,7 @@
         
       #2.2 FROM/TO paths
         from.local_to_groundhog <- paste0(ip.return$LibPath, "/",ip.return$Package)
-        to.local_to_groundhog <- paste0(ip.return$groundhog_location, "/", ip.return$Package)
+        to.local_to_groundhog   <- paste0(ip.return$groundhog_location, "/", ip.return$Package)
         
         
       #2.3 As precaution, delete any destination folder, and create the parent
@@ -31,11 +31,18 @@
           dir.create(dirname(fk),recursive = TRUE,showWarnings = FALSE)
           }
         
-      #2.4 Move back to groundhog 
+      #2.4 Execute
           outcome.return <- file.rename(from.local_to_groundhog , to.local_to_groundhog)
+         
+          #Any failures?
+          if (mean(outcome.return)<1) {
+            message("groundhog says:\nwarning. The following folders failed to be created:\n",pasteQC(to.local_to_groundhog[!outcome.return]))
+          }
           
+           
       #2.5 remove returned packages from loans
         loans<-loans[!loans$md5 %in% ip.return$md5,]  
+        save.loans(loans)  #update .rds (Utils #60.2)
       } #End of 2
       
   #----------------------------------------------------------------------------------
@@ -50,16 +57,30 @@
       
       #3.2 From and To for backups
         from.local_to_backup <- paste0(ip.backup$LibPath,"/",ip.backup$Package)
-        to.local_to_backup   <- paste0(backup.dir,ip.backup$pkg_vrs)
+        to.local_to_backup   <- paste0(backup.dir,ip.backup$pkg_vrs,"/",ip.backup$Package)
       
+        #make directories of type 'pkg_vrs', we then put pkg inside it 
+          dirs=dirname(to.local_to_backup)
+          for (k in 1:length(dirs)) dir.create(dirs[k], recursive=TRUE,showWarnings = FALSE)
+        
       #3.3 Subset that are new
         new <- !file.exists(to.local_to_backup)    #are they new to the backup? (do not replace existing files)
         
       #3.4 Move new backup files
+        #dir.create(to.local_to_backup,recursive=TRUE,showWarnings = FALSE)
         outcome.backup <- file.rename(from.local_to_backup[new] , to.local_to_backup[new])
-      
+         
+        
+          #Any failures?
+          if (mean(outcome.backup)<1) {
+            message("groundhog says:\nwarning. The following folders failed to be created:\n",pasteQC(to.local_to_backup[!outcome.backup]))
+          }
+          
+        
+        
+        
       #3.5 Delete backup files that somehow already are backed up
-        unlink(from.local_to_backup[!new])
+        unlink(from.local_to_backup[!new],recursive = TRUE)
       } #End 3
     
         
