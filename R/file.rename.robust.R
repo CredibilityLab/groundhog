fee619b88fa612576335220b5c299791a186de95
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2023-04-17 17:37:26 +0200

    bug in loop/if in file.rename.robust()

--- R/file.rename.robust.R
+++ R/file.rename.robust.R
@@ -22,80 +22,118 @@
               method<-'copy_and_delete'
             }
 
-     #2 RENAME 
-        if (method=='renaming')
-        {
-        #2.0 Loop making sure number of files 
-          
-
-        #2.1 Loop  attempting to rename to its already existing name from->from
-          jtot <- 30 #how many attempts
-          for (j in 1:jtot) 
+     #2 COPY BY RENAMING: 
+          if (method=='renaming')
           {
+          #2.0 Loop making sure number of files 
             
-          #Pilot renaming to same name
-            outcome.j <- file.rename(from,from)  
-          
-          #When it succeeds, rename to the 'to' name (which actually moves the file)
-            if (outcome.j==TRUE) {
+
+          #2.1 Loop  attempting to rename to its already existing name from->from
+            jtot <- 30 #how many attempts
             
-              #Rename file, early return
-                outcome.n <- file.rename(from, to)
-                return(outcome.n)
-                }  #ENd if TRUE
-          
-          #If FALSE
-            if (outcome.j==FALSE) {
+            #message we will show if we fail to rename
+                msg_copy_instead_of_renaming <- paste0("After ",jtot," attempts, groundhog was not able to copy '",from,"' by renaming it. From now on, ",
+                     "groundhog will rely on the slower method of *moving* (copying-and-deleting) rather renaming package locations, ",
+                     "for this and also future groundhog.library() calls. ",
+                      "To over-rule this decision, and have groundhog try the faster method again in future calls, ",
+                     "run `try.renaming.method.again()`. Note that if it fails again, it will simply again change the ",
+                     "default to copying-and-deleting.")
+              
             
-              #Message if it is taking longer than expected
-                if (j==2) {
-                          message("...waiting for '",from,"' to be ready to be moved...")
-                          if  (regexpr('dropbox', tolower(get.groundhog.folder()))>0) {
-                            message("This delay is likely caused because the groundhog folder is in Dropbox\n",
-                                    "You can change its location with `set.groundhog.folder()`")
-                              } #End if dropbox
+            
+            for (j in 1:jtot) 
+            {
+              
+            #2.2 Pilot renaming to same name
+              outcome.j <- file.rename(from,from)  
+              if (outcome.j==TRUE) break
+              
+            #2.3 If Pilot Failed keep trying for some times\
+              # (cannot rename to own name-->file is not yet ready for copying)
+                if (outcome.j==FALSE) {
+              
+                  #Message if it is taking longer than expected
+                        if (j==2) {
+                            message("...waiting for '",from,"' to be ready to be moved...")
+                            if  (regexpr('dropbox', tolower(get.groundhog.folder()))>0) {
+                              message("This delay is likely caused because the groundhog folder is in Dropbox\n",
+                                      "You can change its location with `set.groundhog.folder()`")
+                                } #End if dropbox
+                      
+                            } #End if j==2
                     
-                          } #End if j==2
+                      if (j>2)  cat("...",j)
                   
-                if (j>2)  cat("...",j)
-          
-              #Wait 5 seconds before retrying
-                  Sys.sleep(10)
-      
-            } #End if FALSE
-          } #End loop
-          
-          #Skip a line so that the ... counter is not 
-              cat("\n")
-          
-        #2.2 If loop ends and we are still here
-          if (j==jtot) {
+                      #Wait 5 seconds before retrying
+                          Sys.sleep(10)
+              
+                     } #End if FALSE
+            
+              } #End loop #2.1
+
+            
+
+        #2.5 If loop ends and we have not attempted file-rename or if we tr and we are still here
+          if (j==jtot & outcome.j==FALSE) {
             
             #Cookie
-              #save.cookie("copy_instead_of_renaming")
+              save.cookie("copy_instead_of_renaming")
           
             #msg
-              # msg <- paste0("After ",jtot," attempts, groundhog was not able to copy '",from,"' by renaming it. From now on, ",
-              #        "groundhog will rely on the slower method of *moving* (copying-and-deleting) rather renaming package locations, ",
-              #        "for this and also future groundhog.library() calls. ",
-              #         "To over-rule this determination, and have groundhog try the faster method again in future calls, ",
-              #        "run `try.renaming.method.again()`. Note that if it fails again, it will simply again change the ",
-              #        "default to copying-and-deleting.")
               
-              #message(format.msg(msg))
-             
-          }#End j=jtot
-        } #End if method = 'renaming
+              gstop(format.msg(msg_copy_instead_of_renaming))
+                   
+              }#End j=jtot
           
-      #3 Copy and delete
+          
+        #2.7 If renaming is attempted (outcome.j) 
+            if (outcome.j==TRUE) {
+            
+              #Rename file
+                file.rename(from, to)
+                
+              #Verify it is copied
+                outcome.n <- basename(to) %in% data.frame(utils::installed.packages(dirname(to)),row.names = NULL, stringsAsFactors = FALSE)$Package
+                
+              #If failed
+                if (outcome.n==FALSE) {
+                    
+                  #If not copied, similar message, stop trying the renaming method
+                        save.cookie("copy_instead_of_renaming")
+                  
+                  #failure message
+                      gstop(format.msg(msg_copy_instead_of_renaming))  
+                      
+                    }
+              #if succeed, return TREU
+                return(outcome.n)
+                
+                }  #End if attemped renaming``
+                
+          } #End if method = 'renaming
+          
+        
+      #-----------------------------------
+            
+            
+      #3 METHOD: Copy and delete
           if (method=='copy_and_delete')
           {
+          if (groundhog.in.dropbox()==TRUE) {
+            message("Reminder: having groundhog folder in Dropbox makes things slower.\n",
+                    "You can use `set.groundhog.folder(<path>)` to change its location.\n",
+                    "You can also run `try.renaming.method.again()` to give the faster appraoch\n",
+                    "another chance.")
+          }
+            
           dir.create(to,recursive = TRUE,showWarnings = FALSE)
-          message1("Attempting to copy ",basename(dirname(from))," from groundhog's library to the default library", dirname(from)," to ",to)
+          message1("Copying ",basename(from)," from groundhog's to R's default library.")
           outcome=file.copy(from,dirname(to),recursive = TRUE)
           if (outcome==TRUE) unlink(from)
-          if (outcome!=TRUE) gstop("Failed copying '",from,"' to '", to , "'")
+          if (outcome!=TRUE) {
+            msg<-paste0("Failed copying '" , from , "' to '" , to , "'")
+            gstop(msg)
           return(outcome)
           }
         }
-   
+   }
